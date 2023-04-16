b756a3c3b9a081e7b1f4f0f94ddc89561b7bb99a
elkowar <5300871+elkowar@users.noreply.github.com>
2023-04-16 20:29:18 +0200

    Update to support not passing a subdomain and passing nested subdomains

--- server.pl
+++ server.pl
@@ -1,5 +1,7 @@
+% Declare the module "server" with the exported predicate "server/1"
 :- module(server, [server/1]).
 
+% Import necessary libraries for the server
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_unix_daemon)).
 :- use_module(library(http/http_dispatch)).
@@ -9,51 +11,66 @@
 :- use_module(library(http/http_client)).
 :- use_module(library(yaml)).
 
-% Server
-
+% Define the server predicate with the http_handler to handle requests
 :- http_handler(root(Path), redir_request(Path), []).
 
+% server/1 predicate starts the server on the specified Port
 server(Port) :-
   http_server(http_dispatch, [port(Port)]).
 
-% Redirect exists
+% redir_request/2 predicate handles the case where a redirect exists
 redir_request(Page, Request) :-
+  % log request
   request_subdomain(Request, Subdomain),
   redirection(Subdomain, Page, Url),
   http_redirect(moved, Url, Request).
 
-% Fallback path
+% redir_request/2 predicate handles the fallback case when no redirect is found
 redir_request(Page, _Request) :-
   format('Status: 404~n'),
   format('Content-type: text/plain~n~n'),
   format('No redirect for page ~a~n', Page).
 
+% request_subdomain/2 predicate extracts the subdomain from the Request.
+% If there is no subdomain, i.e. in `elkowar.dev`, then the subdomain returned is `@`.
 request_subdomain(Request, Subdomain) :-
   member(host(Host), Request),
   domain_subdomain(Host, Subdomain).
 
-domain_subdomain(Domain, Subdomain) :- 
-  split_string(Domain, ".", "", [Subdomain, _|_]).
+domain_parts(Domain, Parts, Count) :-
+  split_string(Domain, ".", "", Parts),
+  length(Parts, Count).
 
-% redirection mappings
+% domain_subdomain/2 predicate splits the domain to obtain the subdomain
+% If there is no subdomain, i.e. in `elkowar.dev`, then the subdomain returned is ``.
+domain_subdomain(Domain, "") :- 
+  domain_parts(Domain, _Parts, 2).
+domain_subdomain(Domain, Subdomain) :-
+  domain_parts(Domain, Parts, Count),
+  Count > 2,
+  % all but the last two elements of the "Parts" list, concatenated with a dot
+  append(Prefix, [_Last, _SecondLast], Parts),
+  atomic_list_concat(Prefix, ".", Subdomain).
 
+
+% links_data_url/1 predicate retrieves the URL of the links data from the environment variable
 links_data_url(Url) :- getenv("HEADLIGHTS_LINKS_URL", Url).
 
+% links_data/1 predicate fetches the links data from the URL and parses it as YAML
 links_data(Data) :-
   links_data_url(Url),
   http_get(Url, Raw, []),
   yaml_read(string(Raw), Data).
 
+% redirection/3 predicate retrieves the URL for the specified SubDomain and Path from the links data
 redirection(SubDomainV, PathV, Url) :-
   value_atom(SubDomainV, SubDomain),
   value_atom(PathV, Path),
   links_data(Yaml),
   Url = Yaml.get(SubDomain).get(Path).
 
-% Utils
-
+% value_atom/2 predicate converts a value to an atom if it is not already an atom
 value_atom(A, A) :- atom(A).
 value_atom(V, A) :- atom_string(A, V).
-
 
 % vim:ft=prolog
